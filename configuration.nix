a50249471d08bb005bed9ded771b31f4b7a7d157
Patrick Großmann <patrickdgro@gmail.com>
2023-01-21 22:31:49 +0100

    feat: Added gpg.conf

--- configuration.nix
+++ configuration.nix
@@ -109,6 +109,7 @@
     wget
     htop
     gcc
+	tree
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
--- users/common/default.nix
+++ users/common/default.nix
@@ -3,17 +3,21 @@
   pkgs,
   ...
 }: {
-  programs.fzf.enable = true;
+	imports = [
+		./zsh.nix
+		];
 
   home.packages = with pkgs; [
     sqlite
     bat
     ripgrep
 	killall
+	fzf
   ];
 
   programs.gpg = {
     enable = true;
+	settings = import ../../data/gpg/gpg.conf.nix;
     scdaemonSettings.disable-ccid = true;
     publicKeys = [
       {
@@ -60,45 +64,4 @@
 	};
   };
 
-  programs.zsh = {
-    enable = true;
-    initExtra = builtins.readFile ../../data/zsh/zshrc;
-    plugins = [
-      {
-        name = "powerlevel10k";
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-        src = pkgs.zsh-powerlevel10k;
-      }
-      {
-        name = "fzf-tab";
-        file = "share/fzf-tab/fzf-tab.plugin.zsh";
-        src = pkgs.zsh-fzf-tab;
-      }
-      {
-        name = "fast-syntax-highlighting";
-        file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
-        src = pkgs.zsh-fast-syntax-highlighting;
-      }
-      {
-        name = "zsh-histdb";
-        file = "sqlite-history.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "larkery";
-          repo = "zsh-histdb";
-          rev = "30797f0";
-          sha256 = "PQIFF8kz+baqmZWiSr+wc4EleZ/KD8Y+lxW2NT35/bg=";
-        };
-      }
-	  {
-		name = "sd";
-		file = "sd.plugin.zsh";
-		src = pkgs.fetchFromGitHub {
-			owner = "ianthehenry";
-			repo = "sd";
-			rev = "v1.1.0";
-			sha256 = "X5RWCJQUqDnG2umcCk5KS6HQinTJVapBHp6szEmbc4U=";
-		};
-	  }
-    ];
-  };
 }
