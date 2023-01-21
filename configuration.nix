5d396a92e1a5ccba548551814d9719e31536fb8d
Patrick Großmann <patrickdgro@gmail.com>
2023-01-21 23:51:42 +0100

    feat: Added htop config

--- configuration.nix
+++ configuration.nix
@@ -109,7 +109,6 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     xterm
     wget
-    htop
     gcc
 	tree
   ];
--- users/common/default.nix
+++ users/common/default.nix
@@ -5,6 +5,7 @@
 }: {
 	imports = [
 		./zsh.nix
+		./htop.nix
 		];
 
   home.packages = with pkgs; [
