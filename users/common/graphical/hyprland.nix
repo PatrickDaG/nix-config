8c817968c191749cc90663240415f1969d1b40cb
Patrick Großmann <patrickdgro@gmail.com>
2023-03-21 21:56:45 +0900

    chore: added icons to waybar

--- users/common/graphical/hyprland.nix
+++ users/common/graphical/hyprland.nix
@@ -18,7 +18,32 @@ in {
         position = "bottom";
         modules-left = ["hyprland/window"];
         modules-center = ["workspaces"];
-        modules-right = ["network" "memory" "backlight" "wireplumber" "battery" "clock" "tray"];
+        modules-right = ["network" "backlight" "wireplumber" "battery" "clock" "tray"];
+
+        battery = {
+          format = "{icon} {capacity}%";
+          format-icons = ["" "" "" "" "" "" "" "" ""];
+        };
+
+        backlight = {
+          device = "intel_backlight";
+          format = "{icon} {percent}%";
+          format-icons = ["" ""];
+          on-scroll-up = "${pkgs.acpilight}/bin/xbacklight +5";
+          on-scroll-down = "${pkgs.acpilight}/bin/xbacklight -5";
+        };
+
+        clock.format = "{:%Y-%m-%d %H:%M}";
+
+        wireplumber = {
+          format = "{icon} {volume}%";
+          format-muted = "";
+          format-icons = ["奄" "奔" "墳"];
+        };
+
+        network = {
+          format = "{ifname}  {bandwidthUpBits}  {bandwidthDownBits}";
+        };
       };
     };
   };
