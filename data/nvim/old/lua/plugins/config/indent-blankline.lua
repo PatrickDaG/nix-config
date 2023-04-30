38e496f5af471e96fb523abbe071ed63614357d3
Patrick Großmann <patrickdgro@gmail.com>
2023-04-30 18:40:26 +0900

    chore: removed old vim config

--- data/nvim/old/lua/plugins/config/indent-blankline.lua
+++
@@ -1,4 +0,0 @@
-require("indent_blankline").setup {
-	buftype_exclude = { "help", "terminal", "packer", "nofile" },
-	filetype_exclude = { "terminal", "lsp-info" },
-}
--- data/nvim/old/lua/plugins/config/onedark.lua
+++
@@ -1,6 +0,0 @@
--- setup onedark
-require("onedark").setup{
-	toggle_style_key = "<nop>",
-}
-
-require("onedark").load()
