6d4dbfd4fc6e36482ecf8015ed9a6f43740bd443
Patrick Großmann <patrickdgr@gmail.com>
2023-05-27 00:39:52 +0900

    fix: passwörter sind nice

--- users/root/default.nix
+++ users/root/default.nix
@@ -10,8 +10,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDZixkix0KfKuq7Q19whS5FQQg51/AJGB5BiNF/7h/LM"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHxD4GOrwrBTG4/qQhm5hoSB2CP7W9g1LPWP11oLGOjQ"
     ];
-    #passwordFile = config.rekey.secrets.root.path;
-    password = "ctie";
+    passwordFile = config.rekey.secrets.root.path;
   };
   home-manager.users.root = {
     imports = [../common];
