598d776a948ed60a8d088bc5fec8c917ac4cd71a
Patrick <patrickdgro@gmail.com>
2023-09-02 12:05:57 +0200

    chore: removed ssd used for windows

--- hosts/desktopnix/fs.nix
+++ hosts/desktopnix/fs.nix
@@ -18,17 +18,6 @@
           ];
         };
       };
-      sata-ssd = {
-        type = "disk";
-        device = "/dev/disk/by-id/${config.secrets.secrets.local.disko.sata-ssd}";
-        content = with lib.disko.gpt; {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            (partLuksZfs "infantry-fighting-vehicle" "infantry-fighting-vehicle" "0%" "100%")
-          ];
-        };
-      };
       sata-hdd = {
         type = "disk";
         device = "/dev/disk/by-id/${config.secrets.secrets.local.disko.sata-hdd}";
@@ -43,10 +32,8 @@
     };
     zpool = with lib.disko.zfs; {
       rpool = defaultZpoolOptions // {datasets = defaultZfsDatasets;};
-      infantry-fighting-vehicle = defaultZpoolOptions // {datasets = {};};
       panzer = defaultZpoolOptions // {datasets = {};};
     };
   };
   boot.initrd.luks.devices.enc-rpool.allowDiscards = true;
-  boot.initrd.luks.devices.enc-infantry-fighting-vehicle.allowDiscards = true;
 }
