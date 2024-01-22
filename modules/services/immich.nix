50ce3e9d8eaf96975952cda8052500138b2d2796
Patrick Großmann <patrickdgro@gmail.com>
2024-01-22 01:15:50 +0100

    feat: immich backups

--- modules/services/immich.nix
+++ modules/services/immich.nix
@@ -46,6 +46,45 @@
     ];
   };
 in {
+  age.secrets.resticpasswd = {
+    generator.script = "alnum";
+  };
+  age.secrets.immichHetznerSsh = {
+    generator.script = "ssh-ed25519";
+  };
+  services.restic.backups = {
+    main = {
+      user = "root";
+      timerConfig = {
+        OnCalendar = "06:00";
+        Persistent = true;
+        RandomizedDelaySec = "3h";
+      };
+      initialize = true;
+      passwordFile = config.age.secrets.resticpasswd.path;
+      hetznerStorageBox = {
+        enable = true;
+        inherit (config.secrets.secrets.global.hetzner) mainUser;
+        inherit (config.secrets.secrets.global.hetzner.users.immich) subUid path;
+        sshAgeSecret = "immichHetznerSsh";
+      };
+      backupPrepareCommand = ''
+        ${pkgs.podman}/bin/podman exec -t immich_postgres pg_dumpall -c -U postgres > /run/immich_dump.sql
+      '';
+      paths = [
+        "${upload_folder}/library"
+        "${upload_folder}/upload"
+        "${upload_folder}/profile"
+        "/run/immich_dump.sql"
+      ];
+      pruneOpts = [
+        "--keep-daily 10"
+        "--keep-weekly 7"
+        "--keep-monthly 12"
+        "--keep-yearly 75"
+      ];
+    };
+  };
   microvm = {
     mem = 1024 * 8;
     vcpu = 12;
