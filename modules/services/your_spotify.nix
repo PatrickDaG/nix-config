6fe5d90427fe5964c6be5258c408040f96c113bd
Patrick <patrick@failmail.dev>
2024-03-02 10:22:02 +0100

    feat: your spotify modules

--- modules/services/your_spotify.nix
+++ modules/services/your_spotify.nix
@@ -1,21 +1,21 @@
 {config, ...}: {
   imports = [./your_spotify_m.nix];
   age.secrets.spotify = {
-    owner = config.services.your_spotify.user;
-    group = config.services.your_spotify.group;
+    inherit (config.services.your_spotify) user group;
     rekeyFile = ../../secrets/your_spotify.age;
   };
   services.your_spotify = {
     enable = true;
     config = {
       clientEndpoint = "https://spotify.${config.secrets.secrets.global.domains.web}";
+      apiEndpoint = "https://api.spotify.${config.secrets.secrets.global.domains.web}";
     };
     environmentFile = config.age.secrets.spotify.path;
   };
   environment.persistence."/persist".directories = [
     {
+      inherit (config.services.mongodb) user;
       directory = config.services.mongodb.dbpath;
-      user = config.services.mongodb.user;
     }
   ];
 }
