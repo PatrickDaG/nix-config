c05a28567096f22e287a13c879ee5dc8d954ad42
Patrick <patrick@failmail.dev>
2024-06-10 15:15:12 +0200

    fix: open-webui prod ENV

--- config/services/ollama.nix
+++ config/services/ollama.nix
@@ -14,12 +14,19 @@
     port = 3000;
     enable = true;
     environment = {
+      ENV = "prod";
       OLLAMA_BASE_URL = "http://localhost:3001";
       ANONYMIZED_TELEMETRY = "False";
       DO_NOT_TRACK = "True";
       SCARF_NO_ANALYTICS = "True";
       TRANSFORMERS_CACHE = "/var/lib/open-webui/cache/huggingface";
       WEBUI_AUTH_TRUSTED_EMAIL_HEADER = "X-Email";
+      ENABLE_COMMUNITY_SHARING = "False";
+      ENABLE_ADMIN_EXPORT = "False";
+
+      WEBUI_AUTH = "False";
+      ENABLE_SIGNUP = "False";
+      DEFAULT_USER_ROLE = "user";
     };
   };
   environment.persistence."/state".directories = [
