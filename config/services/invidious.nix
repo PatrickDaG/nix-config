3833b8abf0d8197b44641e13f5b212200388bf38
Patrick <patrick@failmail.dev>
2024-08-31 14:11:06 +0200

    chore(invidious): better default settings

--- config/services/invidious.nix
+++ config/services/invidious.nix
@@ -6,6 +6,21 @@
     settings = {
       external_port = 443;
       https_only = true;
+      popular_enabled = false;
+      default_user_preferences = {
+        dark_mode = "dark";
+        feed_menu = [
+          "Subscriptions"
+          "Playlists"
+          "Trending"
+        ];
+        default_home = "Subscriptions";
+        player_style = "youtube";
+        quality = "dash";
+        save_player_pos = true;
+        local = true;
+        extend_desc = true;
+      };
     };
   };
   environment.persistence."/persist".directories = [
