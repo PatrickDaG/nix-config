b0a5464af620bd10ed0413f6451865f91d352256
Patrick <patrick@failmail.dev>
2024-11-10 19:13:24 +0100

    chore: update signal-to-blog

--- pkgs/signal-to-blog.nix
+++ pkgs/signal-to-blog.nix
@@ -4,8 +4,8 @@ rustPlatform.buildRustPackage {
 
   src = fetchgit {
     url = "https://forge.lel.lol/patrick/signal-to-blog.git";
-    rev = "bdb7c803d2185ca1d5cd11d21f3606eef34b0555";
-    hash = "sha256-mcgP1u+a63W+gbvIpz0uXqwcd2AOKC+VcDpK0mV6GIg=";
+    rev = "5e7a42d386a76f23affcb3d58b54dd41e96844ec";
+    hash = "sha256-MBKLx67Ivsk7E/6H75xj8zpeXzG+zXGBsEfHzn10YHk=";
   };
 
   cargoHash = "sha256-q9r1VeRQ5HOmBdst58MgS+hdyEXHIdncqV1v3OTmQv8=";
