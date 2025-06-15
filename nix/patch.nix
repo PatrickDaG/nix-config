51acc909a911e19e886c2c4610f1c2417847ec5b
Patrick <patrick@failmail.dev>
2025-06-15 21:42:54 +0200

    chore: switch to applyPatches

--- nix/patch.nix
+++ nix/patch.nix
@@ -9,16 +9,9 @@
         system = "x86_64-linux";
         pkgs = import inputs.nixpkgs { inherit system; };
       in
-      pkgs.stdenvNoCC.mkDerivation {
-        name = "Nixpkgs with patches from open PRs";
+      pkgs.applyPatches {
         src = inputs.nixpkgs;
-        dontConfigure = true;
-        dontBuild = true;
-        doCheck = false;
-        dontFixup = true;
-        installPhase = ''
-          cp -r ./ $out
-        '';
+        name = "nixpkgs-patched";
         patches =
           if builtins.pathExists ../patches then pkgs.lib.filesystem.listFilesRecursive ../patches else [ ];
       };
