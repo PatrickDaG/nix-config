58cab375500c41208aaee96b4a357119ea110879
Patrick <patrick@failmail.dev>
2025-10-09 20:59:43 +0200

    refactor: nginx

--- config/services/nginx.nix
+++ config/services/nginx.nix
@@ -6,23 +6,27 @@
   ...
 }:
 let
+  public = config.node.name == "torweg";
   ipOf =
     name:
-    if name == "firezone" then
+    if public then
       globals.wireguard.services-extern.hosts.${globals.services.${name}.host}.ipv4
     else
       globals.wireguard.services.hosts.${globals.services.${name}.host}.ipv4;
   blockOf =
     hostName:
     {
-      virtualHostExtraConfig ? "",
+      virtualHostExtraConfig ? { },
       maxBodySize ? "500M",
       port ? 3000,
       upstream ? hostName,
       protocol ? "http",
+      proxyProtect ? false,
+      publicAccess ? false,
+      privateAcess ? true,
       ...
     }:
-    {
+    lib.mkIf ((public && publicAccess) || (!public && privateAcess)) {
       upstreams.${hostName} = {
         servers."${ipOf upstream}:${toString port}" = { };
         extraConfig = ''
@@ -34,70 +38,65 @@ let
           expectedBodyRegex = hostName;
         };
       };
-      virtualHosts.${globals.services.${hostName}.domain} = {
-        forceSSL = true;
-        useACMEHost = "web";
-        locations."/" = {
-          proxyPass = "${protocol}://${hostName}";
-          proxyWebsockets = true;
-          X-Frame-Options = "SAMEORIGIN";
-        };
-        extraConfig = ''
-          client_max_body_size ${maxBodySize} ;
-        ''
-        + virtualHostExtraConfig;
-      };
-    };
-  proxyProtect =
-    hostName:
-    {
-      allowedGroup ? true,
-      ...
-    }@cfg:
-    lib.mkMerge [
-      (blockOf hostName cfg)
-      {
-        virtualHosts.${globals.services.${hostName}.domain} = {
-          locations."/".extraConfig = ''
-            auth_request /oauth2/auth;
-            error_page 401 = /oauth2/sign_in;
+      virtualHosts.${globals.services.${hostName}.domain} = lib.mkMerge [
+        {
+          forceSSL = true;
+          useACMEHost = "web";
+          locations = lib.mkMerge (
+            [
+              {
+                "/" = {
+                  proxyPass = "${protocol}://${hostName}";
+                  proxyWebsockets = true;
+                  X-Frame-Options = "SAMEORIGIN";
+                  extraConfig = lib.mkIf proxyProtect ''
+                    auth_request /oauth2/auth;
+                    error_page 401 = /oauth2/sign_in;
 
-            # pass information via X-User and X-Email headers to backend,
-            # requires running with --set-xauthrequest flag
-            auth_request_set $user   $upstream_http_x_auth_request_preferred_username;
-            # Set the email to our own domain in case user change their mail
-            auth_request_set $email  "''${upstream_http_x_auth_request_preferred_username}@${globals.domains.web}";
-            proxy_set_header X-User  $user;
-            proxy_set_header X-Email $email;
+                    # pass information via X-User and X-Email headers to backend,
+                    # requires running with --set-xauthrequest flag
+                    auth_request_set $user   $upstream_http_x_auth_request_preferred_username;
+                    # Set the email to our own domain in case user change their mail
+                    auth_request_set $email  "''${upstream_http_x_auth_request_preferred_username}@${globals.domains.web}";
+                    proxy_set_header X-User  $user;
+                    proxy_set_header X-Email $email;
 
-            # if you enabled --cookie-refresh, this is needed for it to work with auth_request
-            auth_request_set $auth_cookie $upstream_http_set_cookie;
-            add_header Set-Cookie $auth_cookie;
+                    # if you enabled --cookie-refresh, this is needed for it to work with auth_request
+                    auth_request_set $auth_cookie $upstream_http_set_cookie;
+                    add_header Set-Cookie $auth_cookie;
+                  '';
+                };
+              }
+            ]
+            ++ (lib.optional proxyProtect {
+              "/oauth2/" = {
+                proxyPass = "http://oauth2-proxy";
+                extraConfig = ''
+                  proxy_set_header X-Scheme                $scheme;
+                  proxy_set_header X-Auth-Request-Redirect $scheme://$host$request_uri;
+                '';
+              };
+
+              "/oauth2/auth" = {
+                proxyPass = "http://oauth2-proxy/oauth2/auth?allowed_groups=${hostName}_access";
+                extraConfig = ''
+                  internal;
+
+                  proxy_set_header X-Scheme         $scheme;
+                  # nginx auth_request includes headers but not body
+                  proxy_set_header Content-Length   "";
+                  proxy_pass_request_body           off;
+                '';
+              };
+            })
+          );
+          extraConfig = ''
+            client_max_body_size ${maxBodySize} ;
           '';
-          locations."/oauth2/" = {
-            proxyPass = "http://oauth2-proxy";
-            extraConfig = ''
-              proxy_set_header X-Scheme                $scheme;
-              proxy_set_header X-Auth-Request-Redirect $scheme://$host$request_uri;
-            '';
-          };
-
-          locations."= /oauth2/auth" = {
-            proxyPass =
-              "http://oauth2-proxy/oauth2/auth"
-              + lib.optionalString allowedGroup "?allowed_groups=${hostName}_access";
-            extraConfig = ''
-              internal;
-
-              proxy_set_header X-Scheme         $scheme;
-              # nginx auth_request includes headers but not body
-              proxy_set_header Content-Length   "";
-              proxy_pass_request_body           off;
-            '';
-          };
-        };
-      }
-    ];
+        }
+        virtualHostExtraConfig
+      ];
+    };
 in
 {
   globals.wireguard.services.hosts.${config.node.name} = { };
@@ -111,7 +110,6 @@ in
     {
       enable = true;
       recommendedSetup = true;
-      # virtualHosts."${globals.services.netbird.domain}".useACMEHost = "web";
       upstreams.fritz = {
         servers."${lib.net.cidr.host 1 "10.99.2.0/24"}:443" = { };
         extraConfig = ''
@@ -171,16 +169,80 @@ in
           '';
         };
       };
-      virtualHosts.${globals.services.fireflypico.domain}.locations."/api" = {
-        proxyPass = "http://fireflypico";
-        proxyWebsockets = true;
-        X-Frame-Options = "SAMEORIGIN";
+
+    }
+    (lib.mkIf public {
+      upstreams.firezone-api = {
+        servers."${ipOf "firezone"}:${
+          toString nodes.${globals.services.firezone.host}.config.services.firezone.server.api.port
+        }" =
+          { };
         extraConfig = ''
-          proxy_set_header X-User  "";
-          proxy_set_header X-Email "";
+          zone firezone 64k;
+          keepalive 2;
         '';
       };
-      virtualHosts.${globals.services.firefly.domain}.locations."/api" = {
+    })
+    (blockOf "firezone" {
+      publicAccess = true;
+      privateAcess = false;
+      virtualHostExtraConfig.locations."/api/" = {
+        # The trailing slash is important to strip the location prefix from the request
+        proxyPass = "http://firezone-api/";
+        proxyWebsockets = true;
+      };
+    })
+    (blockOf "vaultwarden" { maxBodySize = "1G"; })
+    (blockOf "jellyfin" {
+      maxBodySize = "10G";
+      port = 8096;
+      virtualHostExtraConfig.extraConfig = ''
+        allow ${globals.net.vlans.home.cidrv4} ;
+        allow ${globals.net.vlans.home.cidrv6} ;
+        deny all ;
+      '';
+    })
+    (blockOf "forgejo" { maxBodySize = "1G"; })
+    (blockOf "immich" {
+      maxBodySize = "5G";
+      virtualHostExtraConfig.extraConfig = ''
+        proxy_buffering off;
+        proxy_request_buffering off;
+      '';
+    })
+    (blockOf "adguardhome" {
+      proxyProtect = true;
+    })
+    (blockOf "oauth2-proxy" {
+      port = 3001;
+      allowedGroup = false;
+      proxyProtect = true;
+      virtualHostExtraConfig.locations."/oauth2/auth".proxyPass =
+        lib.mkForce "http://oauth2-proxy/oauth2/auth";
+    })
+    (lib.recursiveUpdate (blockOf "paperless" { maxBodySize = "5G"; }) {
+      # Paperless allowed hosts disallows ip based access
+      # django.core.exceptions.DisallowedHost: Invalid HTTP_HOST header: '10.42.0.10:3000'. You may need to add '10.42.0.10' to ALLOWED_HOSTS.
+      upstreams."paperless".monitoring.enable = false;
+    })
+    (blockOf "ttrss" {
+      port = 80;
+      proxyProtect = true;
+    })
+    (blockOf "invidious" {
+      proxyProtect = true;
+    })
+    (blockOf "yourspotify" { port = 80; })
+    (blockOf "blog" { port = 80; })
+    (blockOf "homeassistant" { })
+    (blockOf "esphome" {
+      port = 3001;
+      proxyProtect = true;
+    })
+    (blockOf "firefly" {
+      port = 80;
+      proxyProtect = true;
+      virtualHostExtraConfig.locations."/api" = {
         proxyPass = "http://firefly";
         proxyWebsockets = true;
         X-Frame-Options = "SAMEORIGIN";
@@ -191,65 +253,23 @@ in
           proxy_set_header X-Email "";
         '';
       };
-
-      upstreams.firezone-api = {
-        servers."${ipOf "firezone"}:${toString nodes.torweg.config.services.firezone.server.api.port}" =
-          { };
+    })
+    (blockOf "fireflypico" {
+      port = 80;
+      proxyProtect = true;
+      virtualHostExtraConfig.locations."/api" = {
+        proxyPass = "http://fireflypico";
+        proxyWebsockets = true;
+        X-Frame-Options = "SAMEORIGIN";
         extraConfig = ''
-          zone firezone 64k;
-          keepalive 2;
+          proxy_set_header X-User  "";
+          proxy_set_header X-Email "";
         '';
       };
-      virtualHosts.${globals.services.firezone.domain} = {
-        locations."/api/" = {
-          # The trailing slash is important to strip the location prefix from the request
-          proxyPass = "http://firezone-api/";
-          proxyWebsockets = true;
-        };
-      };
-    }
-    (blockOf "firezone" { })
-    (blockOf "vaultwarden" { maxBodySize = "1G"; })
-    (blockOf "jellyfin" {
-      maxBodySize = "10G";
-      port = 8096;
-      virtualHostExtraConfig = ''
-        allow ${globals.net.vlans.home.cidrv4} ;
-        allow ${globals.net.vlans.home.cidrv6} ;
-        deny all ;
-      '';
     })
-    (blockOf "forgejo" { maxBodySize = "1G"; })
-    (blockOf "immich" {
-      maxBodySize = "5G";
-      virtualHostExtraConfig = ''
-        proxy_buffering off;
-        proxy_request_buffering off;
-      '';
-    })
-    (proxyProtect "adguardhome" { })
-    (proxyProtect "oauth2-proxy" {
-      port = 3001;
-      allowedGroup = false;
-    })
-    (lib.recursiveUpdate (blockOf "paperless" { maxBodySize = "5G"; }) {
-      # Paperless allowed hosts disallows ip based access
-      # django.core.exceptions.DisallowedHost: Invalid HTTP_HOST header: '10.42.0.10:3000'. You may need to add '10.42.0.10' to ALLOWED_HOSTS.
-      upstreams."paperless".monitoring.enable = false;
-    })
-    (proxyProtect "ttrss" { port = 80; })
-    (proxyProtect "invidious" { })
-    (blockOf "yourspotify" { port = 80; })
-    (blockOf "blog" { port = 80; })
-    #(blockOf "headscale" { })
-    (blockOf "homeassistant" { })
-    #(proxyProtect "ollama" { })
-    (proxyProtect "esphome" { port = 3001; })
-    (proxyProtect "firefly" { port = 80; })
-    (proxyProtect "fireflypico" { port = 80; })
     (blockOf "firefly-data-importer" {
       port = 80;
-      virtualHostExtraConfig = ''
+      virtualHostExtraConfig.extraConfig = ''
         allow ${globals.net.vlans.home.cidrv4} ;
         allow ${globals.net.vlans.home.cidrv6} ;
         deny all ;
@@ -266,7 +286,7 @@ in
     })
     (blockOf "kanidm" {
       protocol = "https";
-      virtualHostExtraConfig = ''
+      virtualHostExtraConfig.extraConfig = ''
         proxy_ssl_verify off ;
       '';
     })
@@ -303,17 +323,4 @@ in
       mode = "0755";
     }
   ];
-
-  # services.netbird.server.proxy =
-  #   let
-  #     cfg = nodes.nucnix-netbird.config.services.netbird.server;
-  #   in
-  #   {
-  #     domain = "${globals.services.netbird.domain}";
-  #     enableNginx = true;
-  #     signalAddress = "${ipOf "netbird"}:${toString cfg.signal.port}";
-  #     relayAddress = "${ipOf "netbird"}:${toString cfg.relay.port}";
-  #     managementAddress = "${ipOf "netbird"}:${toString cfg.management.port}";
-  #     dashboardAddress = "${ipOf "netbird"}:80";
-  #   };
 }
