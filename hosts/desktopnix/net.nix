719ee47ab52899d5791fc26c4754c895c7045422
Patrick <patrick@failmail.dev>
2025-12-31 17:59:59 +0100

    feat: enable iwd on desktop

--- hosts/desktopnix/net.nix
+++ hosts/desktopnix/net.nix
@@ -6,6 +6,9 @@
 {
   networking = {
     inherit (config.secrets.secrets.local.networking) hostId;
+    wireless.iwd = {
+      enable = true;
+    };
   };
   systemd.network.networks = {
     "01-lan1" = {
@@ -22,16 +25,21 @@
         IPv6PrivacyExtensions = "yes";
       };
     };
+    "01-wlan2" = {
+      DHCP = "yes";
+      matchConfig.MACAddress = config.secrets.secrets.local.networking.interfaces.wlan02.mac;
+      networkConfig = {
+        IPv6PrivacyExtensions = "yes";
+      };
+    };
   };
-  networking.nftables.firewall.zones.untrusted.interfaces = [ "lan01" ];
+  networking.nftables.firewall.zones.untrusted.interfaces = [ "lan01" "wlan01" "wlan02"];
   services.mullvad-vpn = {
     enable = true;
     package = pkgs.mullvad-vpn;
   };
   environment.persistence."/state".directories = [
+    "/var/lib/iwd"
     "/etc/mullvad-vpn"
-  ];
-  meta.telegraf.availableMonitoringNetworks = [
-    "home"
   ];
 }
