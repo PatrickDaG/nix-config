650170deba5ef229b29b78090e99e5ad6aafa9c3
Patrick <patrick@failmail.dev>
2026-04-13 16:09:13 +0200

    chore: move locking to nim

--- pkgs/scripts/default.nix
+++ pkgs/scripts/default.nix
@@ -3,16 +3,6 @@ _final: prev: {
   # so we have to name this differently
   pat-scripts = {
     clone-term = prev.callPackage ./clone-term.nix { };
-    unlock = prev.writeShellApplication {
-      name = "unlock-builders";
-      runtimeInputs = [ ];
-      text = builtins.readFile ./unlock.sh;
-    };
-    lock = prev.writeShellApplication {
-      name = "lock-builders";
-      runtimeInputs = [ ];
-      text = builtins.readFile ./lock.sh;
-    };
     update = prev.writeShellApplication {
       name = "update";
       runtimeInputs = [ ];
