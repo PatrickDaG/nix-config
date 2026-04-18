4dcb6aedeed24ec2eb0c1a892ea41716320536ea
Patrick <patrick@failmail.dev>
2026-04-18 14:46:39 +0200

    chore: update mongodb

--- pkgs/mongodb-bin.nix
+++ pkgs/mongodb-bin.nix
@@ -9,23 +9,23 @@
 }:
 stdenv.mkDerivation {
   pname = "mongodb-bin";
-  version = "1.0.0";
+  version = "1.0.1";
   srcs = [
     (fetchurl {
-      url = "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2204-6.0.14.tgz";
-      hash = "sha256-1MW3pVIffdxq63gY64ozM1erWM2ou2L8T+MTfG+ZPLg=";
+      url = "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2404-8.2.7.tgz";
+      hash = "sha256-GYyWeVSoRXgrlQqx7R2chxH3+5S4ewbTefWJR9S2Frs=";
     })
     (fetchurl {
-      url = "https://downloads.mongodb.com/compass/mongosh-2.1.5-linux-x64.tgz";
-      hash = "sha256-R1GGB0ZGqmpJtMUNF2+EJK6iNiChHuoHyOf2vKDcOKA=";
+      url = "https://downloads.mongodb.com/compass/mongosh-2.8.2-linux-x64.tgz";
+      hash = "sha256-nkEtOLbaoTRawo1JnQf4Dooj4aBZO/bD4F9NLUdD6+s=";
     })
   ];
   sourceRoot = ".";
   nativeBuildInputs = [ autoPatchelfHook ];
   buildPhase = ''
     mkdir -p $out/bin
-    cp mongosh-2.1.5-linux-x64/bin/mongosh $out/bin/mongo
-    cp mongodb-linux-x86_64-ubuntu2204-6.0.14/bin/mongod $out/bin/mongod
+    cp mongosh-2.8.2-linux-x64/bin/mongosh $out/bin/mongo
+    cp mongodb-linux-x86_64-ubuntu2404-8.2.7/bin/mongod $out/bin/mongod
   '';
   buildInputs = [
     openssl
